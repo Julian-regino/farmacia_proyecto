5f8acafe1230cf6f950d1d70a0a64e0805858f37
Julian <julianregino862@gmail.com>
2024-09-22 19:39:40 -0500

    comitt 2

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const btnProducto1 = document.getElementById("anadirproducto1");
 
-btnProducto1.addEventListener
+btnProducto1.addEventListener('click', (e)=> {
+    e.preventDefault();
+    alert("Paracetamol-Capsule-500mg añadido al carrito de ventas");
+})
