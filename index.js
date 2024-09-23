6b48dd394ce43dfec72b70268a40996c62f32c88
Julian <julianregino862@gmail.com>
2024-09-22 20:37:39 -0500

    commit 19

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Actualizar carrito de compras
 
 var numProductos = 0;
+var numProducto1 = 0;
+var numProducto2 = 0;
 const btnProducto1 = document.getElementById('anadirproducto1');
 const btnProducto2 = document.getElementById('anadirproducto2');
 const etiquetaNumProducto = document.getElementById('numCarrito');
+const modalBody = document.querySelector('.modal-body');
 
 btnProducto1.addEventListener('click', (e)=> {
     e.preventDefault();
     alert("Paracetamol-Capsule-500mg añadido al carrito de ventas");
     numProductos = numProductos + 1;
     etiquetaNumProducto.innerHTML = numProductos;
+    btnProducto1 = btnProducto1 + 1;
+
 });
 
 btnProducto2.addEventListener('click', (e)=> {
