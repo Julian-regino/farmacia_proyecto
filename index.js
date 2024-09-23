2c284d5d1ed703fac2d92b096fbb79777f643a2c
Julian <julianregino862@gmail.com>
2024-09-22 19:51:12 -0500

    commit 5

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var numProductos = 0;
-const btnProducto1 = document.getElementById("anadirproducto1");
+const btnProducto1 = document.getElementById('anadirproducto1');
+const etiquetaNumProducto = document.getElementById('numCarrito');
 
 btnProducto1.addEventListener('click', (e)=> {
     e.preventDefault();
     alert("Paracetamol-Capsule-500mg añadido al carrito de ventas");
+    numProductos = numProductos + 1;
+    etiquetaNumProducto.innerHTML = numProductos;
 })
 
 function actualizarNumProductos() {
